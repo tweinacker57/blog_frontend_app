fe9fd5475786b3bd33ab4a32aa601940fa0a99b2
Tyler Weinacker <tweinacker57@gmail.com>
2020-12-15 20:32:16 -0500

    Add sign up page, route, and link. Add login page, route, and link

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test.vue'
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +18,16 @@ const routes = [
     path: '/test',
     name: 'Test',
     component: Test
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup
   },
   {
     path: '/about',
